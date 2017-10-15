91b447528129dcc362ffbfb4f9965668305cf11f
tvminhxuyen <tvminhxuyen@gmail.com>
2017-10-16 04:13:22 +0700

    admin silder image + update products

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/minhanhshop/admin/');
-define('HTTP_CATALOG', 'http://localhost/minhanhshop/');
+define('HTTP_SERVER', 'http://localhost/v2/minhanhshop-v2/admin/');
+define('HTTP_CATALOG', 'http://localhost/v2/minhanhshop-v2/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/minhanhshop/admin/');
-define('HTTPS_CATALOG', 'http://localhost/minhanhshop/');
+define('HTTPS_SERVER', 'http://localhost/v2/minhanhshop-v2/admin/');
+define('HTTPS_CATALOG', 'http://localhost/v2/minhanhshop-v2/');
 
 // DIR
-define('DIR_APPLICATION', '/opt/lampp/htdocs/minhanhshop/admin/');
-define('DIR_SYSTEM', '/opt/lampp/htdocs/minhanhshop/system/');
-define('DIR_IMAGE', '/opt/lampp/htdocs/minhanhshop/image/');
-define('DIR_LANGUAGE', '/opt/lampp/htdocs/minhanhshop/admin/language/');
-define('DIR_TEMPLATE', '/opt/lampp/htdocs/minhanhshop/admin/view/template/');
-define('DIR_CONFIG', '/opt/lampp/htdocs/minhanhshop/system/config/');
-define('DIR_CACHE', '/opt/lampp/htdocs/minhanhshop/system/storage/cache/');
-define('DIR_DOWNLOAD', '/opt/lampp/htdocs/minhanhshop/system/storage/download/');
-define('DIR_LOGS', '/opt/lampp/htdocs/minhanhshop/system/storage/logs/');
-define('DIR_MODIFICATION', '/opt/lampp/htdocs/minhanhshop/system/storage/modification/');
-define('DIR_UPLOAD', '/opt/lampp/htdocs/minhanhshop/system/storage/upload/');
-define('DIR_CATALOG', '/opt/lampp/htdocs/minhanhshop/catalog/');
+define('DIR_APPLICATION', 'D:/SERVER/htdocs/v2/minhanhshop-v2/admin/');
+define('DIR_SYSTEM', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/');
+define('DIR_IMAGE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/image/');
+define('DIR_LANGUAGE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/admin/language/');
+define('DIR_TEMPLATE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/admin/view/template/');
+define('DIR_CONFIG', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/config/');
+define('DIR_CACHE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/download/');
+define('DIR_LOGS', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/upload/');
+define('DIR_CATALOG', 'D:/SERVER/htdocs/v2/minhanhshop-v2/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'tt_selena_test');
+define('DB_DATABASE', 'mashop_db');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/minhanhshop/');
+define('HTTP_SERVER', 'http://localhost/v2/minhanhshop-v2/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/minhanhshop/');
+define('HTTPS_SERVER', 'http://localhost/v2/minhanhshop-v2/');
 
 // DIR
-define('DIR_APPLICATION', '/opt/lampp/htdocs/minhanhshop/catalog/');
-define('DIR_SYSTEM', '/opt/lampp/htdocs/minhanhshop/system/');
-define('DIR_IMAGE', '/opt/lampp/htdocs/minhanhshop/image/');
-define('DIR_LANGUAGE', '/opt/lampp/htdocs/minhanhshop/catalog/language/');
-define('DIR_TEMPLATE', '/opt/lampp/htdocs/minhanhshop/catalog/view/theme/');
-define('DIR_CONFIG', '/opt/lampp/htdocs/minhanhshop/system/config/');
-define('DIR_CACHE', '/opt/lampp/htdocs/minhanhshop/system/storage/cache/');
-define('DIR_DOWNLOAD', '/opt/lampp/htdocs/minhanhshop/system/storage/download/');
-define('DIR_LOGS', '/opt/lampp/htdocs/minhanhshop/system/storage/logs/');
-define('DIR_MODIFICATION', '/opt/lampp/htdocs/minhanhshop/system/storage/modification/');
-define('DIR_UPLOAD', '/opt/lampp/htdocs/minhanhshop/system/storage/upload/');
+define('DIR_APPLICATION', 'D:/SERVER/htdocs/v2/minhanhshop-v2/catalog/');
+define('DIR_SYSTEM', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/');
+define('DIR_IMAGE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/image/');
+define('DIR_LANGUAGE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/catalog/language/');
+define('DIR_TEMPLATE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/config/');
+define('DIR_CACHE', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/download/');
+define('DIR_LOGS', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/SERVER/htdocs/v2/minhanhshop-v2/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'tt_selena_test');
+define('DB_DATABASE', 'mashop_db');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
